2a25562f32bebc656bf3229e6b228f6803b73fff
palaciopedro <153301312+palaciopedro@users.noreply.github.com>
2026-02-17 11:38:09 -0300

    initialized jdbc4 and introduced delete method

---
+++ jdbc4/src/application/Program.java
@@ -0,0 +1,36 @@
+package application;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import db.DB;
+import db.DBIntegrityException;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Connection conn = null;
+		PreparedStatement st = null;
+		
+		try {
+			conn = DB.getConnection();
+			
+			st = conn.prepareStatement(
+					"DELETE FROM department "
+					+ "WHERE "
+					+ "Id = ?");
+			
+			st.setInt(1, 2);
+		
+			st.executeUpdate();
+		} catch (SQLException e) {
+			throw new DBIntegrityException(e.getMessage());
+		} finally {
+			DB.closeStatement(st);
+			DB.closeConnection();
+		}
+	}
+
+}
