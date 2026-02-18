1404d79cda759021b5467b167ba5548c17e2bb3b
palaciopedro <153301312+palaciopedro@users.noreply.github.com>
2026-02-18 14:28:01 -0300

    Seller entity class

--- dao_jdbc/src/application/Program.java
+++ dao_jdbc/src/application/Program.java
@@ -1,14 +1,22 @@
 package application;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 
+		DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
 		Department obj = new Department(1, "Books");
-		
-		System.out.println(obj);
+
+		Seller seller = new Seller(1, "Carl", "carl@gmail.com", LocalDate.parse("12/02/1984", fmt), 2000.00, obj);
+
+		System.out.println(seller);
 	}
 
 }
